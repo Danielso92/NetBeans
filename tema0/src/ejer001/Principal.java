2e02d7725292826fdd680a7b4c9b90a159690196
Danielso92 <danielsoriguerivera@gmail.com>
2022-11-24 17:30:27 +0100

    primer commit

---
+++ tema0/src/ejer001/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejer001;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Principal {
+    public static void main (String args){
+        System.aut.println{"hola mundo")            
+    }
+    
+    }
+}
